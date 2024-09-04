5e145e0474a45320e74368d367e1f0b2396d786e
VuongNguyen <enlove2005@gmail.com>
2024-09-04 13:47:02 +0700

    update commit

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const jsonServer = require('json-server');
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-
-const router = jsonServer.router('db.json');
-
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/users/:resource/:id/show": "/:resource/:id",
+    "/products/:resource/:id/show": "/:resource/:id",
+    "/carts/:resource/:id/show": "/:resource/:id",
+    "/orders/:resource/:id/show": "/:resource/:id",
+  })
+);
 server.use(router);
 
-// lang nghe cong cua cua ung dung
-
 server.listen(3000, () => {
-    console.log('JSON Server is running at http://localhost:3000');
-})
+  console.log("JSON Server is running");
+});
 
+// Export the Server API
 module.exports = server;
